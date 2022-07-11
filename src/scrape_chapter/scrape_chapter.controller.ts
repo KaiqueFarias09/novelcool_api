72cdb76d4ada969958847fbd8b381d7b55721271
Kaique <kaiquesouzafarias@outlook.com>
2022-07-10 21:31:53 -0300

    I added an endpoint that make the scrapping of specific chapters

--- src/scrape_chapter/scrape_chapter.controller.ts
+++ src/scrape_chapter/scrape_chapter.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Headers } from '@nestjs/common';
+import { ScrapeChapterService } from './scrape_chapter.service';
 
-@Controller('scrape-chapter')
-export class ScrapeChapterController {}
+@Controller()
+export class ScrapeChapterController {
+  constructor(private scrapeChapterService: ScrapeChapterService) {}
+
+  @Get('/read_manga')
+  async getChapter(@Headers('url') url: string) {
+    return this.scrapeChapterService.scrapeChapter(url);
+  }
+}
--- src/scrape_chapter/scrape_chapter.service.ts
+++ src/scrape_chapter/scrape_chapter.service.ts
@@ -1,4 +1,46 @@
 import { Injectable } from '@nestjs/common';
 
+import { CheerioAPI, load } from 'cheerio';
+import axios from 'axios';
+
 @Injectable()
-export class ScrapeChapterService {}
+export class ScrapeChapterService {
+  async scrapeChapter(url: string) {
+    try {
+      let chapterPage = await axios.get(`${url}-10-1html`);
+      let scrappedPage = load(chapterPage.data);
+
+      const data = [];
+      let chapterHasPages = true;
+      while (chapterHasPages) {
+        const nextBtnLink = scrappedPage(
+          'div.mangaread-pagenav:nth-child(1) > div:nth-child(6) > a:nth-child(1)',
+        ).attr('href');
+
+        scrappedPage('div.pic_box > img').each((i, el) => {
+          data.push({
+            img: scrappedPage(el).attr('src'),
+          });
+        });
+
+        chapterHasPages = this.thereIsMorePages(nextBtnLink);
+        if (chapterHasPages) {
+          chapterPage = await axios.get(nextBtnLink);
+          scrappedPage = load(chapterPage.data);
+        }
+      }
+
+      return data;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  private thereIsMorePages(nextBtnLink: string) {
+    if (nextBtnLink === '/') {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
